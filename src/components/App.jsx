2959157bcf3e1066b034b7d5ae38c591f151f089
Serhii Pochtovyk <sergey.po4tovik@gmail.com>
2023-10-30 13:43:36 +0200

    add local storage;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,24 @@ import Filter from './Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(stringifiedContacts) || [];
+    this.setState({
+      contacts: parsedContacts
+    });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      const stringifiedContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', stringifiedContacts);
+    }
+  }
 
   addContact = (name, number) => {
     const isNameExists = this.state.contacts.some(
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+
 class ContactForm extends Component {
   state = {
     name: '',
